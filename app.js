80131de14f2a60472f52bb01d5c6c117eb355bf0
Hy-Quynh <hyquynh123@gmail.com>
2021-03-23 10:36:09 +0700

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-const sumRouter=require('./routers/plus');
+const sumRouter=require('./routers/sum');
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true }));
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({extended:true }));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.use('/plus', sumRouter);
+app.use('/sum', sumRouter);
 
 
-const port =process.env.PORT || 3003;
+const port =process.env.PORT || 3000;
 console.log(`Sever is listening on port ${port}`);
 app.listen(port)
